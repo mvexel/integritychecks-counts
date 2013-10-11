fff369524e1ef46b49ff4b747794d36beade9077
Martijn van Exel <m@rtijn.org>
2013-10-11 17:31:19 -0600

    add one more query, commenting queries

--- perform_quantitative_checks.sh
+++ perform_quantitative_checks.sh
@@ -55,10 +55,18 @@ fi
 # * counttype - a string of 20 chars or less representing the type of count
 # * the count query, output must be a hstore. 
 QUERIES=()
+# this query counts the highway ways by type
 QUERIES+=("insert into $SCHEMA.counts values (now(), 'highwaycount', (select hstore(array_agg(t::text),array_agg(c::text)) from (select tags->'highway' as t, count(1) as c from ways where tags?'highway' and (linestring && Box2d(st_geomfromtext('linestring(-124.7625 24.5210, -66.9326 49.3845)')) or linestring && Box2d(st_geomfromtext('linestring(-179.1506 51.2097, -129.9795 71.4410)')) or linestring && Box2d(st_geomfromtext('linestring(-160.2471 18.9117, -154.8066 22.2356)'))) group by tags->'highway') foo));")
+# this query sums the highway lengths by type
 QUERIES+=("insert into $SCHEMA.counts values (now(), 'highwaylength', (select hstore(array_agg(t::text),array_agg(c::text)) from (select tags->'highway' as t, sum(st_length(st_transform(linestring, 3786))) as c from ways where tags?'highway' and (linestring && Box2d(st_geomfromtext('linestring(-124.7625 24.5210, -66.9326 49.3845)')) or linestring && Box2d(st_geomfromtext('linestring(-179.1506 51.2097, -129.9795 71.4410)')) or linestring && Box2d(st_geomfromtext('linestring(-160.2471 18.9117, -154.8066 22.2356)'))) group by tags->'highway') foo));")
+# this query counts the route relations by network
 QUERIES+=("insert into $SCHEMA.counts values (now(), 'relationcount', (select hstore(array_agg(t::text),array_agg(c::text)) from (select tags->'network' t, count(1) c from relations where tags->'type' = 'route' and tags->'route' = 'road' and tags?'network' group by tags->'network') foo));")
+# this query counts the highway nodes by type (highway=traffic_signals etc)
 QUERIES+=("insert into $SCHEMA.counts values (now(), 'highwaynodecount', (select hstore(array_agg(t::text), array_agg(c::text)) from (select tags->'highway' as t, count(1) as c from nodes where tags?'highway' and (geom && Box2d(st_geomfromtext('linestring(-124.7625 24.5210, -66.9326 49.3845)')) or geom && Box2d(st_geomfromtext('linestring(-179.1506 51.2097, -129.9795 71.4410)')) or geom && Box2d(st_geomfromtext('linestring(-160.2471 18.9117, -154.8066 22.2356)'))) group by tags->'highway') foo));")
+# this query counts the amenity nodes by type (pois)
+QUERIES+=("insert into $SCHEMA.counts values (now(), 'amenitynodecount', (select hstore(array_agg(t::text), array_agg(c::text)) from (select tags->'amenity' as t, count(1) as c from nodes where tags?'amenity' and (geom && Box2d(st_geomfromtext('linestring(-124.7625 24.5210, -66.9326 49.3845)')) or geom && Box2d(st_geomfromtext('linestring(-179.1506 51.2097, -129.9795 71.4410)')) or geom && Box2d(st_geomfromtext('linestring(-160.2471 18.9117, -154.8066 22.2356)'))) group by tags->'amenity') foo));")
+# this query counts the shop nodes by type (pois)
+QUERIES+=("insert into $SCHEMA.counts values (now(), 'shopnodecount', (select hstore(array_agg(t::text), array_agg(c::text)) from (select tags->'shop' as t, count(1) as c from nodes where tags?'shop' and (geom && Box2d(st_geomfromtext('linestring(-124.7625 24.5210, -66.9326 49.3845)')) or geom && Box2d(st_geomfromtext('linestring(-179.1506 51.2097, -129.9795 71.4410)')) or geom && Box2d(st_geomfromtext('linestring(-160.2471 18.9117, -154.8066 22.2356)'))) group by tags->'shop') foo));")
 echo "----------------------------------------------------------"
 echo "start: `date`"
 for ((q = 0; q < ${#QUERIES[@]}; q++))
